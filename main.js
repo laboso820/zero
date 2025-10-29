4da42f74ba92c3cfcc089a2389af1217b4fd4290
laboso820 <obadiahlaboso820@gmail.com>
2025-10-29 06:58:42 +0300

    expense file add

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log("ths is my javascrpt file")
+// document.title="thebee"
+// buttonEl=document.getElementById("btn")
+// const modeEl=document.getElementById("mode")
+
+// // buttonEl.addEventListener("click",function(){
+// //     alert("you are now started")
+// // })
+// modeEl.addEventListener("click", function(){
+//     if(document.body.style.backgroundColor==="white"){
+    
+//     document.body.style.backgroundColor="green"
+//     } 
+//     else{
+//         document.body.style.backgroundColor="blue"
+//     }
+      
+    
+
+// })
+nameInput=document.getElementById("name");
+amountInput=document.getElementById("amount");
+let buttonEl=document.getElementById("btn");
+
+buttonEl.addEventlistener("click",function(){
+    alert("you have zerobalance");
+});
